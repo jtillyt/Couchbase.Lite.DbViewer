30fb70292fe4c01fa530bca84db0d9f3f6695c32
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2021-05-21 13:05:49 -0500

    Merge pull request #3 from RLittlesII/feature/mac-hub-viewer
    feature: Add macOS app to start DbViewer.Hub

---
+++ src/DbViewer.Hub.macOS/ViewController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using AppKit;
+using Foundation;
+
+namespace DbViewer.Hub.macOs
+{
+    public partial class ViewController : NSViewController
+    {
+        NSAttributedString titleString = new NSAttributedString("Make\nEpic\nThings",
+            new NSStringAttributes()
+            {
+                ParagraphStyle = new NSMutableParagraphStyle()
+                {
+                    LineHeightMultiple = 0.75f
+                }
+            });
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get { return base.RepresentedObject; }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}

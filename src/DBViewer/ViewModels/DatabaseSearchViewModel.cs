05d7625f3075697753cbc1fa2d72ab3ba03a02f0
James Thomas <jt@jaybirdlabs.com>
2023-06-11 08:30:57 -0500

    wip (not working on ios)

--- src/DBViewer/ViewModels/DatabaseSearchViewModel.cs
+++ src/DBViewer/ViewModels/DatabaseSearchViewModel.cs
@@ -8,24 +8,22 @@ using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Couchbase.Lite;
 using Dawn;
 using DbViewer.DataStores;
 using DbViewer.Extensions;
 using DbViewer.Models;
 using DbViewer.Views;
-using DevLab.JmesPath;
 using DynamicData;
 using Newtonsoft.Json;
 using Prism.Navigation;
 using ReactiveUI;
+using Couchbase.Lite;
+using MoonSharp.Interpreter;
 
 namespace DbViewer.ViewModels
 {
     public class DatabaseSearchViewModel : NavigationViewModelBase, INavigatedAware
     {
-
-
         public DatabaseSearchViewModel(IDatabaseDatastore cacheService, INavigationService navigationService)
             : base(navigationService)
         {
@@ -47,9 +45,9 @@ namespace DbViewer.ViewModels
 
             ViewSelectedDocumentCommand = ReactiveCommand.Create<DocumentModel>(ExecuteViewSelectedDocument);
 
-            this.WhenAnyValue(vm=>vm.UseAdvancedSearch)
-                .Select(val=>val?"JMES Query (ex.. FieldName=='val'&&FieldName2=='val2')":"Search Text")
-                .BindTo(this, x=>x.PlaceholderText)
+            this.WhenAnyValue(vm => vm.UseAdvancedSearch)
+                .Select(val => val ? "JMES Query (ex.. FieldName=='val'&&FieldName2=='val2')" : "Search Text")
+                .BindTo(this, x => x.PlaceholderText)
                 .DisposeWith(Disposables);
         }
 
@@ -79,6 +77,12 @@ namespace DbViewer.ViewModels
         {
             get => _placeHolderText;
             set => this.RaiseAndSetIfChanged(ref _placeHolderText, value);
+        }
+
+        public double Progress
+        {
+            get => _progress;
+            set => this.RaiseAndSetIfChanged(ref _progress, value);
         }
 
         public ReadOnlyObservableCollection<DocumentModel> SearchResults => _searchResults;
@@ -133,8 +137,10 @@ namespace DbViewer.ViewModels
 
         private Task<List<string>> SearchDbAsync(string searchText, CancellationToken cancellationToken)
         {
-            return Task.Run(() =>
+            return Task.Run(async () =>
             {
+                UpdateProgress(0);
+
                 var documentIdsWithHits = new List<string>();
 
                 var database = CurrentDatabaseItemViewModel.Database;
@@ -148,15 +154,40 @@ namespace DbViewer.ViewModels
                 var connection = database.ActiveConnection;
 
                 var documentIds = connection.ListAllDocumentIds();
+                var totalDocCount = Math.Min(documentIds.Count, MaxResults);
 
+                //try
+                //{
+                //    using (var document = connection.GetDocumentById(documentIds[0]))
+                //    {
+                //        var cleanedDocument = document.CleanAttachments();
+                //        var copileResult = UseScript(searchText, cleanedDocument);
+                //    }
+                //}
+                //catch (Exception ex)
+                //{
+                //    Debug.WriteLine(ex);
+                //}
+
+                //return new List<string>();
 
                 var count = 0;
-                foreach (var documentId in documentIds)
+                for (int i = 0; i < documentIds.Count; i++)
                 {
+                    var documentId = documentIds[i];
+
                     cancellationToken.ThrowIfCancellationRequested();
 
                     using (var document = connection.GetDocumentById(documentId))
                     {
+
+                        try
+                        {
+                            //var copileResult = UseScript(searchText, document);
+                        }
+                        catch (Exception ex)
+                        {
+                        }
 
                         var documentText = string.Empty;
 
@@ -176,24 +207,52 @@ namespace DbViewer.ViewModels
                             if (IsMatch(documentText, searchText))
                             {
                                 documentIdsWithHits.Add(documentId);
-                                count++;
-
-                                if (count == 500)
-                                {
-                                    break;
-                                }
                             }
                         }
                         catch (Exception ex)
                         {
                             Debug.WriteLine("Search cancelled due to exception");
+
+                            UpdateProgress(0);
+
                             break;
                         }
+                        finally
+                        {
+                            count++;
+                            UpdateProgress(count / totalDocCount);
+                        }
+                    }
+
+                    if (documentIdsWithHits.Count >= MaxResults)
+                    {
+                        break;
                     }
                 }
 
                 return documentIdsWithHits;
             });
+        }
+
+        private bool UseScript(string query, Document document)
+        {
+            UserData.RegisterType<ScriptDocument>();
+
+            var cleanedQuery = query;
+
+            if (!query.StartsWith("return "))
+            {
+                cleanedQuery = "return " + cleanedQuery;
+            }
+
+            Script script = new Script();
+            DynValue obj = UserData.Create(new ScriptDocument(document));
+
+            script.Globals["obj"] = obj;
+
+            DynValue res = script.DoString(cleanedQuery);
+
+            return res.Boolean;
         }
 
         private bool IsMatch(string documentText, string searchText)
@@ -211,20 +270,26 @@ namespace DbViewer.ViewModels
         {
             try
             {
-                var jmes = new JmesPath();
-                var result = jmes.Transform(documentText, searchText);
+                ////https://jmespath.org/specification.html#id16
+                ////https://github.com/jdevillard/JmesPath.Net
+                //var jmes = new JmesPath();
 
-                if (result == null)
-                {
-                    return false;
-                }
+                //var ast = jmes.Parse(searchText);
 
-                if (bool.TryParse(result, out bool boolValue))
-                {
-                    return boolValue;
-                }
+                //var result = jmes.Transform(documentText, searchText);
 
-                return false;
+                //if (result == null)
+                //{
+                //    return false;
+                //}
+
+                //if (bool.TryParse(result, out bool boolValue))
+                //{
+                //    return boolValue;
+                //}
+
+                return true;
+                //return !string.IsNullOrWhiteSpace(result);
             }
             catch (Exception ex)
             {
@@ -232,6 +297,8 @@ namespace DbViewer.ViewModels
                 throw;
             }
         }
+
+
 
         private void ExecuteViewSelectedDocument(DocumentModel document)
         {
@@ -246,7 +313,16 @@ namespace DbViewer.ViewModels
             });
         }
 
+        private void UpdateProgress(double progress)
+        {
+            RunOnUi(() =>
+            {
+                Progress = progress;
+            });
+        }
+
         internal const string DocumentIdListParam = nameof(DocumentIdListParam);
+        private const int MaxResults = 500;
 
         private readonly IDatabaseDatastore _cacheService;
         private bool _useAdvancedSearch;
@@ -255,6 +331,7 @@ namespace DbViewer.ViewModels
         private string _searchTitle;
         private string _searchText = "";
         private string _placeHolderText = "Search Text";
+        private double _progress = 0.0;
 
         private readonly ReadOnlyObservableCollection<DocumentModel> _searchResults;
 
---
+++ src/DbViewer/Models/ScriptDocument.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Couchbase.Lite;
+using MoonSharp.Interpreter;
+
+namespace DbViewer.Models
+{
+    [MoonSharpUserData]
+    public class ScriptDocument
+    {
+
+        private Document _document;
+        private Dictionary<string, object> _dictionary;
+        public ScriptDocument(Document document)
+        {
+            _document = document;
+            _dictionary = document.ToDictionary();
+        }
+
+        public object this[string field]
+        {
+            get { return _dictionary[field]; }
+            set { _dictionary[field] = value; }
+        }
+    }
+}
